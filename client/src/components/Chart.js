50f4a9a3275f58ba791a60c76b491e24071a6bf5
ericong18 <ericong18@gmail.com>
2018-07-13 11:31:49 -0700

    Removed extra words that came with lyric data

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -3,23 +3,40 @@ import './Chart.css';
 
 let ZingChart = require('zingchart-react').core;
 
-
 class Chart extends Component {
 
   render() {
     return (
-      <ZingChart 
-        height="300" 
-        width="600" 
-        data={
-          { 
-            type: 'wordcloud',
-            options: {
-              text: 'hello'
-            } 
-          }
-        } 
-      />
+      <div className="chart-container">
+        <ZingChart 
+          id="myChart"
+          height="500" 
+          width="800"
+          theme="dark"
+          data={
+            { 
+              type: 'wordcloud',
+              options: {
+                text: this.props.lyrics,
+                maxItems: 100,
+                aspect: 'spiral',
+                colorType: 'palette',
+                palette: ['#1db954', '#0b6623', '#9dc183', '#c7ea46', '#3f704d', '#00A86B', '#8F9779', '#A9BA9D', '#98FB98', '#01796F', '#D0F0C0', '#00A572', '#4B5320', '#50C878', '#39FF14'],
+                style: {
+                  fontFamily: 'Catamaran',
+                  hoverState: {
+                    alpha: 1,
+                    backgroundColor: 'black',
+                    borderColor: 0,
+                    fontColor: 'white',
+                    textAlpha: 1,
+                  }
+                }
+              } 
+            }
+          } 
+        />
+      </div>
     );
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-// import Chart from './Chart';
+import Chart from './Chart';
 import axios from 'axios';
 import './Home.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-
-let ZingChart = require('zingchart-react').core;
 
 class Home extends Component {
 
@@ -24,25 +22,24 @@ class Home extends Component {
     return name.substring(0, name.search(" "))
   }
 
+  removeSpecialWords(lyric) {
+    let revised = lyric.replace(/This Lyrics is NOT for Commercial use/g, '');
+    revised = revised.replace(/\(1409617797032\)/g, '');
+    revised = revised.replace(/\*/g, '');
+    revised = revised.replace(/\n/g, ' ');
+    revised = revised.replace(/...     /g, '');
+    revised = revised.replace(/  /g, ' ');
+    revised = revised.toLowerCase();
+
+    return revised;
+  }
+
   componentDidMount() {
     let arr = this.props.topTracks.items;
     let trackMap = [];
     for (let i = 0; i < arr.length; ++i) {
       trackMap.push([arr[i].name, arr[i].artists[0].name])
     }
-
-    console.log(API_KEY);
-
-    // axios.get(`https://cors-anywhere.herokuapp.com/` + `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=${trackMap[0][0]}&q_artist=${trackMap[0][1]}&apikey=${API_KEY}`, {
-    //   headers: { 
-    //     'Accept': 'application/json',
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    //     'Access-Control-Allow-Headers' : 'Content-Type'
-    //   }})
-    //   .then(res => {
-    //     console.log(res);
-    // })
 
     let topLyrics = "";
     for (let j = 0; j < trackMap.length; ++j) {
@@ -51,10 +48,9 @@ class Home extends Component {
 
       axios.get(`https://cors-anywhere.herokuapp.com/` + `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback&q_track=${trackTitle}&q_artist=${trackArtist}&apikey=${API_KEY}`)
         .then(res => {
-          console.log(res);
           if (res.data.message.body.lyrics) {
-            // console.log(res.data.message.body.lyrics.lyrics_body)
             topLyrics = topLyrics + res.data.message.body.lyrics.lyrics_body;
+            topLyrics = this.removeSpecialWords(topLyrics);
             this.setState({topLyrics});
           }
         })
@@ -69,7 +65,7 @@ class Home extends Component {
           <h1 id="greetings">{ this.chooseGreeting() } { this.props.userData.display_name ? this.getFirstName(this.props.userData.display_name) : this.props.userData.id }<span className="green">,</span></h1>
         </div>
         <div className="container-fluid info-container">
-          <p className="subtitle" id="song-desc">Here are your top <span className="green">3</span> tracks on <span className="green">Spotify.</span> Keep scrolling down for more<span className="green">.</span></p>
+          <p className="subtitle" id="song-desc">These are your top <span className="green">3</span> tracks on <span className="green">Spotify.</span> Keep scrolling down for more<span className="green">.</span></p>
           <div className="row">
             <div className="col-4">
               <div className="album-art-container">
@@ -90,43 +86,11 @@ class Home extends Component {
               </div>
             </div>
           </div>
+        </div>
+        <div className="container-fluid chart-container">
           <div className="row">
-            <p className="subtitle" id="chart-desc">These are the <span className="green">lyrics</span> that appear most frequently in your top <span className="green">Spotify</span> tracks<span className="green">;</span> have a look<span className="green">!</span></p>
-            <div className="chart-container">
-              <ZingChart 
-                id="myChart"
-                height="500" 
-                width="800"
-                theme="dark"
-                data={
-                  { 
-                    type: 'wordcloud',
-                    options: {
-                      text: this.state.topLyrics,
-                      maxItems: 50,
-                      aspect: 'spiral',
-                      colorType: 'palette',
-                      palette: ['#1db954', '#0b6623', '#9dc183', '#c7ea46', '#3f704d', '#00A86B', '#8F9779', '#A9BA9D', '#98FB98', '#01796F', '#D0F0C0', '#00A572', '#4B5320', '#50C878', '#39FF14'],
-                      style: {
-                        fontFamily: 'Catamaran',
-                        tooltip: {
-                          text: '%text: %hits',
-                          visible: true,
-                          alpha: 0.9,
-                          backgroundColor: 'black',
-                          borderColor: 'none',
-                          borderRadius: 2,
-                          fontColor: 'white',
-                          fontFamily: 'Arial',
-                          fontSize: 14,
-                          textAlpha: 1,
-                        }
-                      }
-                    } 
-                  }
-                } 
-              />
-            </div>
+            <p className="subtitle" id="chart-desc">Here are the <span className="green">lyrics</span> that appear the most in your top <span className="green">Spotify</span> tracks<span className="green">;</span> have a look<span className="green">!</span></p>
+            <Chart lyrics={ this.state.topLyrics } />
           </div>
         </div>
       </div>
